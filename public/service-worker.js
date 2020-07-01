f2082a593edae3d8012791207ae443606e7aa22e
Travis Cultreri <travis@travis-x.lan>
2020-07-01 16:56:52 -0400

    removed files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "/index.js",
+    // "/index.html",
+    // "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/manifest.webmanifest",
